99bca9133f47ec5a910879aced5f8b8d080e0dfc
MedSveta <sveta1978medved@gmail.com>
2025-11-20 19:27:26 +0200

    TypeForm, Gender, DateOfBirth

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -1,5 +1,9 @@
 package pages;
 
+import dto.Student;
+import enums.Gender;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -22,13 +26,34 @@ public class PracticeFormPage extends BasePage {
     WebElement inputEmail;
     @FindBy(id = "userNumber")
     WebElement inputMobile;
+    @FindBy(id = "dateOfBirthInput")
+    WebElement fieldDateOfBirth;
 
-    public void typePracticeForm(){
+    public void typePracticeForm(Student student){
        hideBanner();
        hideFooter();
-       inputName.sendKeys("Sveta");
-       inputLastName.sendKeys("Svetlova");
-       inputEmail.sendKeys("qwerty123@gmail.com");
-       inputMobile.sendKeys("0123456789");
+       inputName.sendKeys(student.getName());
+       inputLastName.sendKeys(student.getLastName());
+       inputEmail.sendKeys(student.getEmail());
+       typeGender(student.getGender());
+       inputMobile.sendKeys(student.getMobile());
+       typeDateOfBirth(student.getDateOfBirth());
+    }
+
+    private void typeGender(Gender gender){
+        WebElement btnGender = driver.findElement(By.xpath(gender.getLocator()));
+        btnGender.click();
+    }
+
+    private void typeDateOfBirth(String dateOfBirth){
+        fieldDateOfBirth.click();
+        String operationSystem = System.getProperty("os.name");
+        System.out.println(operationSystem);
+        if (operationSystem.startsWith("Win"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        else if (operationSystem.startsWith("Mac"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+        fieldDateOfBirth.sendKeys(dateOfBirth);
+        fieldDateOfBirth.sendKeys(Keys.ENTER);
     }
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import dto.Student;
+import enums.Gender;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
@@ -10,8 +12,11 @@ public class PracticeFormTests extends AppManager {
 
     @Test
     public void studentRegFormPositiveTest() {
+        Student student = new Student("Vasya","Vasilev", "vasyavasilev@gmail.com",
+                Gender.MALE, "0123456789", "23 Dec 2006", "Maths,Physics,Chemistry",
+                "Music", "", "address 1", "NCR", "Delhi");
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPracticeForm();
-        new PracticeFormPage(getDriver()).typePracticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
     }
 }
