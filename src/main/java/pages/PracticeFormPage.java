2b7068500fbdf80cacfb1e9ece03f947d880f774
MedSveta <sveta1978medved@gmail.com>
2025-11-24 19:15:39 +0200

    Subjects, Hobbies, StateCity

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import dto.Student;
 import enums.Gender;
+import enums.Hobbies;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.security.Key;
+import java.util.List;
 
 public class PracticeFormPage extends BasePage {
     public PracticeFormPage(WebDriver driver) {
@@ -28,24 +32,80 @@ public class PracticeFormPage extends BasePage {
     WebElement inputMobile;
     @FindBy(id = "dateOfBirthInput")
     WebElement fieldDateOfBirth;
+    @FindBy(id = "subjectsInput")
+    WebElement inputSubjects;
+    @FindBy(xpath = "//textarea[@placeholder='Current Address']")
+    WebElement inputAddress;
+    @FindBy(id = "react-select-3-input")
+    WebElement inputState;
+    @FindBy(id = "react-select-4-input")
+    WebElement inputCity;
+    @FindBy(id = "submit")
+    WebElement btnSubmit;
+    @FindBy(id = "example-modal-sizes-title-lg")
+    WebElement modalMessage;
 
-    public void typePracticeForm(Student student){
-       hideBanner();
-       hideFooter();
-       inputName.sendKeys(student.getName());
-       inputLastName.sendKeys(student.getLastName());
-       inputEmail.sendKeys(student.getEmail());
-       typeGender(student.getGender());
-       inputMobile.sendKeys(student.getMobile());
-       typeDateOfBirth(student.getDateOfBirth());
+
+    public void typePracticeForm(Student student) {
+        hideBanner();
+        hideFooter();
+        inputName.sendKeys(student.getName());
+        inputLastName.sendKeys(student.getLastName());
+        inputEmail.sendKeys(student.getEmail());
+        typeGender(student.getGender());
+        inputMobile.sendKeys(student.getMobile());
+        typeDateOfBirth(student.getDateOfBirth());
+        typeSubjects(student.getSubjects());
+        typeHobbies(student.getHobbies());
+        inputAddress.sendKeys(student.getAddress());
+        typeStateCity(student.getState(), student.getCity());
+        btnSubmit.click();
+
     }
 
-    private void typeGender(Gender gender){
+    public boolean validateModalMessage(String text){
+        return validateTextInElement(modalMessage, text);
+    }
+
+    private void typeStateCity(String state, String city) {
+        inputState.sendKeys(state);
+        inputState.sendKeys(Keys.ENTER);
+
+        inputCity.sendKeys(city);
+        inputCity.sendKeys(Keys.ENTER);
+    }
+
+    private void typeHobbies(List<Hobbies> hobbies) {
+        for (Hobbies h : hobbies) {
+            switch (h) {
+                case MUSIC:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case SPORTS:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case READING:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+            }
+        }
+    }
+
+    private void typeSubjects(String subjects) {
+        inputSubjects.click();
+        String[] arr = subjects.split(",");
+        for (String s : arr) {
+            inputSubjects.sendKeys(s);
+            inputSubjects.sendKeys(Keys.ENTER);
+        }
+    }
+
+    private void typeGender(Gender gender) {
         WebElement btnGender = driver.findElement(By.xpath(gender.getLocator()));
         btnGender.click();
     }
 
-    private void typeDateOfBirth(String dateOfBirth){
+    private void typeDateOfBirth(String dateOfBirth) {
         fieldDateOfBirth.click();
         String operationSystem = System.getProperty("os.name");
         System.out.println(operationSystem);
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -2,21 +2,33 @@ package tests;
 
 import dto.Student;
 import enums.Gender;
+import enums.Hobbies;
+import enums.StateCity;
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeFormTests extends AppManager {
 
     @Test
     public void studentRegFormPositiveTest() {
-        Student student = new Student("Vasya","Vasilev", "vasyavasilev@gmail.com",
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.READING);
+        hobbies.add(Hobbies.SPORTS);
+        Student student = new Student("Vasya", "Vasilev", "vasyavasilev@gmail.com",
                 Gender.MALE, "0123456789", "23 Dec 2006", "Maths,Physics,Chemistry",
-                "Music", "", "address 1", "NCR", "Delhi");
+                hobbies, "", "address 1", StateCity.UTTAR_PRADESH.getState(),
+                StateCity.UTTAR_PRADESH.getCity()[1]);
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPracticeForm();
         new PracticeFormPage(getDriver()).typePracticeForm(student);
+        Assert.assertTrue(new PracticeFormPage(getDriver())
+                .validateModalMessage("Thanks for submitting the form"));
     }
 }
